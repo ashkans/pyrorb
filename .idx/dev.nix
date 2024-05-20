618c1ec03d1cbf3eefad17ab5b80c595f1017f06
ashkan.shokri <ashkan.shokri@gmail.com>
2024-05-20 04:48:26 +0000

    add some new setting for dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -11,6 +11,8 @@
     # pkgs.python311Packages.pip
     # pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
+    pkgs.python311
+    pkgs.poetry
   ];
 
   # Sets environment variables in the workspace
